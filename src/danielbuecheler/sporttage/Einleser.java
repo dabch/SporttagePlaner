6b2f0979ad28415635a50f594ce190a08a7d397b
Daniel Bücheler <dbahn25@t-online.de>
2014-11-09 17:26:18 +0100

    ein unnötiger import entfernt

--- src/danielbuecheler/sporttage/Einleser.java
+++ src/danielbuecheler/sporttage/Einleser.java
@@ -1,6 +1,5 @@
 package danielbuecheler.sporttage;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
