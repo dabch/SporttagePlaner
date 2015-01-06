5f5d700bd404d4a6f4cae8226034bb4c7b901d31
Daniel Bücheler <dbahn25@t-online.de>
2015-01-06 20:16:14 +0100

    Import entfernt

--- src/danielbuecheler/sporttage/ZeitMaker.java
+++ src/danielbuecheler/sporttage/ZeitMaker.java
@@ -3,7 +3,6 @@ package danielbuecheler.sporttage;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Calendar;
 
