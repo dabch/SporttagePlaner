d769126c38a9e083fc2015d1ee357857c0028c14
sansha <sansha98@gmail.com>
2015-03-01 17:43:34 +0100

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,8 +1,8 @@
 <?p
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$servername = 'wp052.webpack.hosteurope.de';
+$username = 'db1093417-sport';
+$password = '%&SporTTage14@!';
+$dbname = 'db1093417-sporttage';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,15 +12,32 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+function isInDatabase($name, $vorname, $klasse) {
+    $abfrageSchueler = $conn->prepare("SELECT ID, Vorname, Name FROM Mannschaften_MS WHERE Vorname = ? AND Name = ? AND Klasse = ?");
+    $abfrageSchueler->bind_param("sss", $vorname, $name, $klasse);
+    $abfrageSchueler->execute();
+}
+
 // prepare and bind
-$stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, class, fb, vb, tt, ) VALUES (?, ?, ?)");
-$stmt->bind_param("sss", $firstname, $lastname, $email);
+$insertSchueler = $conn->prepare("INSERT INTO MyGuests (ID, Name, Vorname, Klasse, BM, FB, BB, VB, TT, ST, FT) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+$insertSchueler->bind_param("sss", $id, $name, $vorname, $klasse, $bm, $fb, $bb, $vb, $tt, $st, $ft);
 
 // set parameters and execute
+$name = $_POST['']
+$vorname
+$klasse
+$bm
+$fb
+$bb
+$vb
+$tt
+$st
+$ft
+
 $firstname = "John";
 $lastname = "Doe";
 $email = "john@example.com";
-$stmt->execute();
+$insertSchueler->execute();
 
 echo "New records created successfully";
 
