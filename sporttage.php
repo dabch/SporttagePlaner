cc3542a8984849c67a66e04707369ec4550c731d
Daniel Bücheler <dbahn25@t-online.de>
2015-03-01 16:03:47 +0100

    initial commit

---
+++ sporttage.php
@@ -0,0 +1,103 @@
+<html>
+<head>
+<meta charset="UTF-8" name="viewport" content="width=device-width" />
+<title>Eingabe Klassenlisten @ Sporttage-Kepler</title>
+</head>
+
+<style>
+table, th, td {
+	border: 1px solid black;
+	border-collapse: collapse;
+}
+
+th, td {
+	padding: 3px;
+}
+</style>
+
+<body>
+Klasse eingeben:
+<form action="" method="post">
+
+<table border="1">
+	<tr>
+		<td colspan="2"> Fußball 1
+		<td colspan="2"> Fußball 2
+		<td colspan="2"> Fahrradtour
+	</tr>
+	<tr>
+		<td> <b>Vorname
+		<td> <b>Nachname
+		<td> <b>Vorname
+		<td> <b>Nachname
+	</tr>
+	
+	<tr>
+		<td> <input type="text" name="fb1_v[]"/>
+		<td> <input type="text" name="fb1_n[]"/>
+		<td> <input type="text" name="fb2_v[]"/>
+		<td> <input type="text" name="fb2_n[]"/>
+	</tr>
+	
+	<tr>
+		<td> <input type="text" name="fb1_v[]"/>
+		<td> <input type="text" name="fb1_n[]"/>
+		<td> <input type="text" name="fb2_v[]"/>
+		<td> <input type="text" name="fb2_n[]"/>
+	</tr>
+	
+	<tr>
+		<td> <input type="text" name="fb1_v[]"/>
+		<td> <input type="text" name="fb1_n[]"/>
+		<td> <input type="text" name="fb2_v[]"/>
+		<td> <input type="text" name="fb2_n[]"/>
+	</tr>
+	
+	<tr>
+		<td> <input type="text" name="fb1_v[]"/>
+		<td> <input type="text" name="fb1_n[]"/>
+		<td> <input type="text" name="fb2_v[]"/>
+		<td> <input type="text" name="fb2_n[]"/>
+	</tr>
+	
+	<tr>
+		<td> <input type="text" name="fb1_v[]"/>
+		<td> <input type="text" name="fb1_n[]"/>
+		<td> <input type="text" name="fb2_v[]"/>
+		<td> <input type="text" name="fb2_n[]"/>
+	</tr>
+	
+	<tr>
+		<td> <input type="text" name="fb1_v[]"/>
+		<td> <input type="text" name="fb1_n[]"/>
+		<td> <input type="text" name="fb2_v[]"/>
+		<td> <input type="text" name="fb2_n[]"/>
+	</tr>
+	
+	<tr>
+		<td> <input type="text" name="fb1_v[]"/>
+		<td> <input type="text" name="fb1_n[]"/>
+		<td> <input type="text" name="fb2_v[]"/>
+		<td> <input type="text" name="fb2_n[]"/>
+	</tr>
+	
+	<tr>
+		<td> <input type="text" name="fb1_v[]"/>
+		<td> <input type="text" name="fb1_n[]"/>
+		<td> <input type="text" name="fb2_v[]"/>
+		<td> <input type="text" name="fb2_n[]"/>
+	</tr>
+	
+	<tr>
+		<td> <input type="text" name="fb1_v[]"/>
+		<td> <input type="text" name="fb1_n[]"/>
+		<td> <input type="text" name="fb2_v[]"/>
+		<td> <input type="text" name="fb2_n[]"/>
+	</tr>
+</table>
+  <input type="submit" name="submit">
+</form>
+<br>
+</body>
+</html>
+
