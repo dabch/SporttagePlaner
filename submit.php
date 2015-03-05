118fb4522fe9d0b161395be37faf1668f60cbc3b
sansha <sansha98@gmail.com>
2015-03-05 16:19:25 +0100

    fixed bug where sportart wasn't updated

--- submit.php
+++ submit.php
@@ -25,13 +25,13 @@
 	$schuelerHinzufuegen = $conn->prepare('INSERT INTO Mannschaften_MS (Vorname, Name, Klasse) VALUES (?, ?, ?);');
 
 	function getSchuelerID($name, $vorname, $klasse) {
-	   // echo $name;
-	    //echo $vorname;
-	    //echo $klasse;
+	  //nun lokale variable damit man sie schließen und öffnen kann
+    /*  $GLOBALS['abfrageSchueler'] = $conn->prepare('SELECT ID FROM Mannschaften_MS WHERE Vorname = ? AND Name = ? AND Klasse = ?');
 	    $GLOBALS['abfrageSchueler']->bind_param('sss', $vorname, $name, $klasse);
 	    $GLOBALS['abfrageSchueler']->execute();
 	    $GLOBALS['abfrageSchueler']->bind_result($id);
 	    if($GLOBALS['abfrageSchueler']->fetch()) {
+        $GLOBALS['abfrageSchueler']->close;
 	    	return $id;
 	    } else {
 	    	$GLOBALS['schuelerHinzufuegen']->bind_param('sss', $vorname, $name, $klasse);
@@ -39,6 +39,25 @@
 	    	$GLOBALS['abfrageSchueler']->execute();
 	    	$GLOBALS['abfrageSchueler']->bind_result($id);
 	    	if($GLOBALS['abfrageSchueler']->fetch()) {
+          $GLOBALS['abfrageSchueler']->close();
+		    	return $id;
+	    	} 
+	    }
+      */
+      $abfrageSchueler = $GLOBALS['conn']->prepare('SELECT ID FROM Mannschaften_MS WHERE Vorname = ? AND Name = ? AND Klasse = ?');
+	    $abfrageSchueler->bind_param('sss', $vorname, $name, $klasse);
+	    $abfrageSchueler->execute();
+	    $abfrageSchueler->bind_result($id);
+	    if($abfrageSchueler->fetch()) {
+        $abfrageSchueler->close;
+	    	return $id;
+	    } else {
+	    	$GLOBALS['schuelerHinzufuegen']->bind_param('sss', $vorname, $name, $klasse);
+	    	$GLOBALS['schuelerHinzufuegen']->execute();
+	    	$abfrageSchueler->execute();
+	    	$abfrageSchueler->bind_result($id);
+	    	if($abfrageSchueler->fetch()) {
+          $abfrageSchueler->close();
 		    	return $id;
 	    	} 
 	    }
@@ -56,20 +75,39 @@
 	echo $klasse . '<br>';
 	 $id = 0;
    $mannschaft = $klasse .  '1';
-  $addSportartToSchueler = $conn->prepare('UPDATE Mannschaften_MS SET FB = ? WHERE ID = ?');
-    $addSportartToSchueler->bind_param('si', $mannschaft, $id);
+  //$addSportartToSchueler = $conn->prepare('UPDATE Mannschaften_MS SET FB = ? WHERE ID = ?;');
+  $addSportartToSchueler = "UPDATE Mannschaften_MS SET FB = " . $mannschaft . " WHERE ID = ". $id . " ";
+ // $addSportartToSchueler2 = $conn->prepare('INSERT INTO Mannschaften_MS (FB) VALUES (?) WHERE ID = ?');
+ // $addSportartToSchueler->bind_param('si', $mannschaft, $id);
+  //$addSportartToSchueler->bind_param('si', $mannschaft, $id);
   
+   $mannschaft= '"' . $mannschaft .  '"';
+   
 	for($i = 0; $i < count($vornamen); $i++) {
     if ($vornamen[$i] != '' && $nachnamen[$i] != '')  {
-    $id = getSchuelerID($nachnamen[$i], $vornamen[$i], $klasse);
+      $id = getSchuelerID($nachnamen[$i], $vornamen[$i], $klasse);
    
-    echo $id;
-    echo $mannschaft;
-    $addSportartToSchueler->bind_param('ss', $mannschaft, $id);
-  //  echo $addSportartToSchueler;
-    $addSportartToSchueler->execute();
-		echo $vornamen[$i] . ' ';
-		echo $nachnamen[$i] . '<br>';
+      echo $id;
+   // echo gettype($id);
+  //  echo gettype($mannschaft);
+      echo $mannschaft;
+   //   $mannschaft= '"' . $mannschaft .  '"';
+      $addSportartToSchueler = "UPDATE Mannschaften_MS SET FB = " . $mannschaft . " WHERE ID = ". $id . " ";
+      echo $mannschaft;
+  //  $addSportartToSchueler->bind_param("si", $mannschaft, $id);
+      echo $addSportartToSchueler;
+      //$conn->query($addSportartToSchueler);
+      //$whatever = $conn->use_result();
+      //$conn->free();
+      if ($conn->query($addSportartToSchueler) === TRUE) {
+        echo "update succesfully";
+      } else {
+        echo "error: " . $conn->error;
+      }
+   // echo $addSportartToSchueler->execute();
+   // echo $addSportartToSchueler->rowCount() . "records UPDATED succesflly";
+		  echo $vornamen[$i] . ' ';
+		  echo $nachnamen[$i] . '<br>';
     
     }
 
