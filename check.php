2296d917f5547fb4b2faad0f5f33315336ae06c5
sansha via PC <sansha98@gmail.com>
2015-03-25 21:45:25 +0100

    major update, now with text input fields

--- check.php
+++ check.php
@@ -1,200 +1,491 @@
-<!DOCTYPE html>
-<?php session_start(); ?>
 <html>
 <head>
-<meta charset='UTF-8' name='viewport' content='width=device-width' />
+<meta charset="UTF-8" name="viewport" content="width=device-width" />
 <title>Eingabe Klassenlisten @ Sporttage-Kepler</title>
 </head>
 
+<style>
+table, th, td {
+	border: 1px solid black;
+	border-collapse: collapse;
+}
+
+th, td {
+	padding: 3px;
+}
+</style>
+
 <body>
-	<h1>Eingaben überprüfen</h1>
-	<?php
-	
-	$klasse = $_POST['klasse'];
-  //k1 in session speichern
-  $k1 = $_POST['k1'];
-	$_SESSION['k1'] = $k1;
-  
-	// Klasse ausgeben und in Session speichern
-	echo 'Klasse: ';
-  if($k1) {
-  echo "K1-";
-  }
-  echo $klasse . '<br>';
-	echo '<br>';
+BITTE ALLES NOCHMALS KONTROLLIEREN!!! <br>
+KEINE LEERZEICHEN benutzen!<br>
+Umlaute können problemlos verarbeitet werden, aber bitte darauf achten, dass Namen nicht falsch geschrieben werden (oder wenn falsch, dann einheitlich falsch)<br>
+Zu einem Vornamen gehört IMMER ein Nachname, sonst geht die Welt unter!<br>
+<br>
+<?php 
+//alle variablen speichern
+$klasse = $_POST['klasse'];
+//k1 in session speichern
+$k1 = $_POST['k1'];
 
-	$_SESSION['klasse'] = $klasse;
-	
-	//Fußball1
-	$vornamen = $_POST['fb1_v'];
-	$nachnamen = $_POST['fb1_n'];
-	$_SESSION['fb1_v'] = $vornamen; // In session abspeichern
-	$_SESSION['fb1_n'] = $nachnamen;
-  
-  
-	echo '<b> Fußball 1 </b><br>';
-  
-  $namenZaehler=0;  //zaehler erstellen
-	for($i = 0; $i < count($vornamen); $i++) {
-		if ($vornamen[$i] != '' && $nachnamen[$i] != '')  {
-			echo $vornamen[$i] . ' ';
-			echo $nachnamen[$i] . '<br>';
-      $namenZaehler++;  //zaehler+1 je mannschaftsteam
-		}
-	}
-  //testen ob die richtige anzahl vorhanden ist, sonst fehlermeldung
-  if ($namenZaehler>0 && $namenZaehler < 6) {
-      	echo '<b><font color="#FF0000"> Achtung: zu wenig Schüler in diesem Team (FB braucht mind 6)</font> </b><br>';
-  }
+$sportartenName = array(
+  0 => "fb1_v[]",
+  1 => "fb1_n[]",
+  2 => "fb2_v[]", 
+  3 => "fb2_n[]",
+  4 => "bb_v[]",
+  5 => "bb_n[]", 
+  6 => "ft_v[]", 
+  7 => "ft_n[]"
+     );
+
+ /*    alt
+$sportarten1['fb1_v[]'] = $_POST['fb1_v'];
+//echo $sportarten1['fb1_v[]']['0'];
+$sportarten1['fb1_n[]'] = $_POST['fb1_n'];
+
+$sportarten1['fb2_v[]'] = $_POST['fb2_v'];
+$sportarten1['fb2_n[]'] = $_POST['fb2_n'];
+
+$sportarten1['bb_v[]'] = $_POST['bb_v'];
+$sportarten1['bb_n[]'] = $_POST['bb_n'];
+
+$sportarten1['ft_v[]'] = $_POST['ft_v'];
+$sportarten1['ft_n[]'] = $_POST['ft_n'];
+
+$sportarten1['bm_v[]'] = $_POST['bm_v'];
+$sportarten1['bm_n[]'] = $_POST['bm_n'];
+
+$sportarten1['tt_v[]'] = $_POST['tt_v'];
+$sportarten1['tt_n[]'] = $_POST['tt_n'];
+
+$sportarten1['vb_v[]'] = $_POST['vb_v'];
+$sportarten1['vb_n[]'] = $_POST['vb_n'];
+
+$sportarten1['st_v[]'] = $_POST['st_v'];
+$sportarten1['st_n[]'] = $_POST['st_n'];
+*/
+//neu:
+//echo $sportarten1[0];
+$sportarten1[0] = $_POST['fb1_v'];
+/*echo $sportarten1['fb1_v']['0'];
+ echo $sportarten1[0];
+ echo $sportarten1['fb1_v'];
+ echo $sportarten1[$sportarten1[0]];
+echo $sportarten1[$sportarten1[0]][0]; */
+ 
+$sportarten1[1] = $_POST['fb1_n'];
+
+$sportarten1[2] = $_POST['fb2_v'];
+$sportarten1[3] = $_POST['fb2_n'];
+
+$sportarten1[4] = $_POST['bb_v'];
+$sportarten1[5] = $_POST['bb_n'];
+
+$sportarten1[6] = $_POST['ft_v'];
+$sportarten1[7] = $_POST['ft_n'];
+
+$sportarten1[8] = $_POST['bm_v'];
+$sportarten1[9] = $_POST['bm_n'];
+
+$sportarten1[10] = $_POST['tt_v'];
+$sportarten1[11] = $_POST['tt_n'];
+
+$sportarten1[12] = $_POST['vb_v'];
+$sportarten1[13] = $_POST['vb_n'];
+
+$sportarten1[14] = $_POST['st_v'];
+$sportarten1[15] = $_POST['st_n'];
+
+?>
+
   
 
-	echo '<br>';
-	
-	//Fußball2
-	$vornamen = $_POST['fb2_v'];
-	$nachnamen = $_POST['fb2_n'];
-	$_SESSION['fb2_v'] = $vornamen;
-	$_SESSION['fb2_n'] = $nachnamen;
-	
-	echo '<b> Fußball 2 </b><br>';
-  $namenZaehler=0; 
-	for($i = 0; $i < count($vornamen); $i++) {
-		if ($vornamen[$i] != '' && $nachnamen[$i] != '')  {
-			echo $vornamen[$i] . ' ';
-			echo $nachnamen[$i] . '<br>';
-      $namenZaehler++;
-		}
-	}
-  if ($namenZaehler>0 && $namenZaehler < 6) {
-      	echo '<b><font color="#FF0000"> Achtung: zu wenig Schüler in diesem Team (FB braucht mind 6)</font> </b><br>';
+Klasse eingeben:
+<form action="submit.php" method="post">
+<input type="checkbox" name="k1" value="k1"> K1  <br>
+<input type="text" name="klasse" required="required" value="<?= $klasse ?> " />
+
+<table>
+	<!-- Fußball 1 + 2 & Fahrradtour -->
+	<tr>
+		<td colspan="2"> Fußball 1
+		<td colspan="2"> Fußball 2		
+		<td colspan="2"> Basketball
+		<td colspan="2"> Fahrradtour
+	</tr>
+	<tr>
+		<td> <b>Vorname
+		<td> <b>Nachname
+		<td> <b>Vorname
+		<td> <b>Nachname
+		<td> <b>Vorname
+		<td> <b>Nachname
+		<td> <b>Vorname
+		<td> <b>Nachname
+	</tr>
+
+ <?php 
+
+  $tabindex = array(1, 2, 19, 20, 37, 38, 55, 56);
+  
+  echo $tabindex[1];
+ //die länge hängt mit der länge von fb1_v zusammen
+  
+  for($zeile = 0; $zeile<9; $zeile++) {
+    echo "<tr> \n";
+    for($i = 0; $i<8; $i++)  {
+      //echo "in der for schleife";
+      echo "<td> <input type=\"text\" name=\"" . $sportartenName[$i] . "\"  value=\"" .  $sportarten1[$i][$zeile] .  "\" tabindex=" . $tabindex[$i] . "/> \n"; 
+      $tabindex[$i]+=2; //tabindexwert um 2 erhöhen für schönes eintippen  
+    }
+    echo "</tr> \n"; 
   }
-	echo '<br>';
+ 
+  
+?> 
+    
+	  
+  
+
+	<!-- Badminton & Tischtennis & Volleyball -->
 	
-	// Basketball
-	$vornamen = $_POST['bb_v'];
-	$nachnamen = $_POST['bb_n'];
-	$_SESSION['bb_v'] = $vornamen; // In session abspeichern
-	$_SESSION['bb_n'] = $nachnamen;
+	<tr>
+		<td colspan="8">  	
+	</tr>
+		<td colspan="2"> Badminton
+		<td colspan="2"> Tischtennis
+		<td colspan="2"> Volleyball
+		<td colspan="2"> Staffellauf
+	</tr>
 	
-	echo '<b> Basketball </b><br>';
-  $namenZaehler=0;  
-	for($i = 0; $i < count($vornamen); $i++) {
-		if ($vornamen[$i] != '' && $nachnamen[$i] != '')  {
-			echo $vornamen[$i] . ' ';
-			echo $nachnamen[$i] . '<br>';
-      $namenZaehler++;
-		}
-	}
-  if ($namenZaehler>0 && $namenZaehler < 6) {
-      	echo '<b><font color="#FF0000"> Achtung: zu wenig Schüler in diesem Team (BB braucht mind 6)</font> </b><br>';
-  }
-	echo '<br>';
-	echo '<br>';
+	<tr>
+		<td> <b>Vorname
+		<td> <b>Nachname
+		<td> <b>Vorname
+		<td> <b>Nachname
+		<td> <b>Vorname
+		<td> <b>Nachname
+		<td> <b>Vorname
+		<td> <b>Nachname
+	</tr>
+	
+	<!-- Team1 -->
+	<tr>
+		<td colspan="2" style="text-align:center"> Team 1
+		<td colspan="2" style="text-align:center"> Team 1
 		
-	// Volleyball
-	$vornamen = $_POST['vb_v'];
-	$nachnamen = $_POST['vb_n'];
-	$_SESSION['vb_v'] = $vornamen; // In session abspeichern
-	$_SESSION['vb_n'] = $nachnamen;
+		<td> <input type="text" name="vb_v[]"/>
+		<td> <input type="text" name="vb_n[]"/>
+		<td> <input type="text" name="st_v[]"/>
+		<td> <input type="text" name="st_n[]"/>
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+		
+		<td> <input type="text" name="vb_v[]"/>
+		<td> <input type="text" name="vb_n[]"/>
+		<td> <input type="text" name="st_v[]"/>
+		<td> <input type="text" name="st_n[]"/>
+	<tr>
 	
-	echo '<b> Volleyball </b><br>';
-  $namenZaehler=0;  
-	for($i = 0; $i < count($vornamen); $i++) {
-		if ($vornamen[$i] != '' && $nachnamen[$i] != '')  {
-			echo $vornamen[$i] . ' ';
-			echo $nachnamen[$i] . '<br>';
-      $namenZaehler++; 
-		}
-	}
-  if ($namenZaehler>0 && $namenZaehler < 6) {
-      	echo '<b><font color="#FF0000"> Achtung: zu wenig Schüler in diesem Team (VB braucht mind 6)</font> </b><br>';
-  }
-	echo '<br>';
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+		
+		<td> <input type="text" name="vb_v[]"/>
+		<td> <input type="text" name="vb_n[]"/>
+		<td> <input type="text" name="st_v[]"/>
+		<td> <input type="text" name="st_n[]"/>
+	<tr>
 	
-	// Staffellauf
-	$vornamen = $_POST['st_v'];
-	$nachnamen = $_POST['st_n'];
-	$_SESSION['st_v'] = $vornamen; // In session abspeichern
-	$_SESSION['st_n'] = $nachnamen;
+	<!-- Team2 -->
+	<tr>
+		<td colspan="2" style="text-align:center"> Team 2
+		<td colspan="2" style="text-align:center"> Team 2
+		
+		<td> <input type="text" name="vb_v[]"/>
+		<td> <input type="text" name="vb_n[]"/>
+		<td> <input type="text" name="st_v[]"/>
+		<td> <input type="text" name="st_n[]"/>
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+		
+		<td> <input type="text" name="vb_v[]"/>
+		<td> <input type="text" name="vb_n[]"/>
+		<td> <input type="text" name="st_v[]"/>
+		<td> <input type="text" name="st_n[]"/>
+	<tr>
 	
-	echo '<b> Staffellauf </b><br>';
-  $namenZaehler=0;     
-	for($i = 0; $i < count($vornamen); $i++) {
-		if ($vornamen[$i] != '' && $nachnamen[$i] != '')  {
-			echo $vornamen[$i] . ' ';
-			echo $nachnamen[$i] . '<br>';
-      $namenZaehler++; 
-		}
-	}
-  if ($namenZaehler>0 && $namenZaehler < 4 ) {
-      	echo '<b><font color="#FF0000"> Achtung: zu wenig Schüler in diesem Team (Staffellauf braucht mind 4)</font> </b><br>';
-  }
-	echo '<br>';
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+		
+		<td> <input type="text" name="vb_v[]"/>
+		<td> <input type="text" name="vb_n[]"/>
+		<td> <input type="text" name="st_v[]"/>
+		<td> <input type="text" name="st_n[]"/>
+	<tr>
 	
-	// Badminton
-	$vornamen = $_POST['bm_v'];
-	$nachnamen = $_POST['bm_n'];
-	$_SESSION['bm_v'] = $vornamen; // In session abspeichern
-	$_SESSION['bm_n'] = $nachnamen;
+	<!-- Team3 -->
+	<tr>
+		<td colspan="2" style="text-align:center"> Team 3
+		<td colspan="2" style="text-align:center"> Team 3
+		
+		<td> <input type="text" name="vb_v[]"/>
+		<td> <input type="text" name="vb_n[]"/>
+		<td> <input type="text" name="st_v[]"/>
+		<td> <input type="text" name="st_n[]"/>
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+		
+		<td> <input type="text" name="vb_v[]"/>
+		<td> <input type="text" name="vb_n[]"/>
+		<td> <input type="text" name="st_v[]"/>
+		<td> <input type="text" name="st_n[]"/>
+	<tr>
 	
-	echo '<b> Badminton </b><br>';
-	$mannschaftsNummer=0;
-     	for($i = 1; $i < count($vornamen); $i+=2) {
-		$mannschaftsNummer++;
-		if ($vornamen[$i] != '' && $nachnamen[$i] != '' && $vornamen[$i - 1] != '' && $nachnamen[$i - 1] != '')  {
-	        	echo 'Team ' . $mannschaftsNummer . ': ';
-			// Spieler 1
-			echo $vornamen[$i] . ' ';
-			echo $nachnamen[$i] . ' &';
-			// Spieler 2
-			echo $vornamen[$i - 1] . ' ';
-			echo $nachnamen[$i - 1] . '<br>';
-		}
-	}
-	echo '<br>';
-
-	// Tischtennis
-	$vornamen = $_POST['tt_v'];
-	$nachnamen = $_POST['tt_n'];
-	$_SESSION['tt_v'] = $vornamen; // In session abspeichern
-	$_SESSION['tt_n'] = $nachnamen;
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+		
+		<td> <input type="text" name="vb_v[]"/>
+		<td> <input type="text" name="vb_n[]"/>
+		<td> <input type="text" name="st_v[]"/>
+		<td> <input type="text" name="st_n[]"/>
+	<tr>
+		
+	<!-- Team4 -->
+	<tr>
+		<td colspan="2" style="text-align:center"> Team 4
+		<td colspan="2" style="text-align:center"> Team 4
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
 	
-	echo '<b> Tischtennis </b><br>';
-	$mannschaftsNummer=0;
-     	for($i = 1; $i < count($vornamen); $i+=2) {
-		$mannschaftsNummer++;
-		if ($vornamen[$i] != '' && $nachnamen[$i] != '' && $vornamen[$i - 1] != '' && $nachnamen[$i - 1] != '')  {
-	        	echo 'Team ' . $mannschaftsNummer . ': ';
-			// Spieler 1
-			echo $vornamen[$i] . ' ';
-			echo $nachnamen[$i] . ' & ';
-			// Spieler 2
-			echo $vornamen[$i - 1] . ' ';
-			echo $nachnamen[$i - 1] . '<br>';
-		}
-	}
-	echo '<br>';
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+		
+	<!-- Team5 -->
+	<tr>
+		<td colspan="2" style="text-align:center"> Team 5
+		<td colspan="2" style="text-align:center"> Team 5
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
 	
-	// Fahrradtour
-	$vornamen = $_POST['ft_v'];
-	$nachnamen = $_POST['ft_n'];
-	$_SESSION['ft_v'] = $vornamen; // In session abspeichern
-	$_SESSION['ft_n'] = $nachnamen;
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
 	
-	echo '<b> Fahrradtour </b><br>';
-   
-	for($i = 0; $i < count($vornamen); $i++) {
-		if ($vornamen[$i] != '' && $nachnamen[$i] != '')  {
-			echo $vornamen[$i] . ' ';
-			echo $nachnamen[$i] . '<br>';
-		}
-	}
-	echo '<br>';
+	
+		
+	<!-- Team6 -->
+	<tr>
+		<td colspan="2" style="text-align:center"> Team 6
+		<td colspan="2" style="text-align:center"> Team 6
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+	
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
   
-  ?>
+  	<!-- Team7 -->
+	<tr>
+		<td colspan="2" style="text-align:center"> Team 7
+		<td colspan="2" style="text-align:center"> Team 7
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
 	
-	<form action='submit.php' method='post'>
-		<input type='submit' value='Alles richtig!'>
-	</form>
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+  
+  	<!-- Team8 -->
+	<tr>
+		<td colspan="2" style="text-align:center"> Team 8
+		<td colspan="2" style="text-align:center"> Team 8
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+	
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+  
+  	<!-- Team9 -->
+	<tr>
+		<td colspan="2" style="text-align:center"> Team 9
+		<td colspan="2" style="text-align:center"> Team 9
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+	
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+  
+  	<!-- Team10 -->
+	<tr>
+		<td colspan="2" style="text-align:center"> Team 10
+		<td colspan="2" style="text-align:center"> Team 10
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+	
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+  
+  	<!-- Team11 -->
+	<tr>
+		<td colspan="2" style="text-align:center"> Team 11
+		<td colspan="2" style="text-align:center"> Team 11
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+	
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+  
+  	<!-- Team12 -->
+	<tr>
+		<td colspan="2" style="text-align:center"> Team 12
+		<td colspan="2" style="text-align:center"> Team 12
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+	
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+  
+  	<!-- Team13 -->
+	<tr>
+		<td colspan="2" style="text-align:center"> Team 13
+		<td colspan="2" style="text-align:center"> Team 13
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+	
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+	
+    	<!-- Team14 -->
+	<tr>
+		<td colspan="2" style="text-align:center"> Team 14
+		<td colspan="2" style="text-align:center"> Team 14
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+	
+	</tr>
+		<td> <input type="text" name="bm_v[]"/>
+		<td> <input type="text" name="bm_n[]"/>
+		<td> <input type="text" name="tt_v[]"/>
+		<td> <input type="text" name="tt_n[]"/>
+	<tr>
+  
+  
+</table>
+  <input type="submit" name="submit">
+</form>
+<br>
+<?php
+	$servername = 'wp052.webpack.hosteurope.de';
+	$username = 'db1093417-sport';
+	$password = '%&SporTTage14@!';
+	$dbname = 'db1093417-sporttage';
+  //ini_set("session.use_trans_sid",true);
+	//session_start();
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	
+	if($conn->connect_error){
+		die('Connection failed:\n' . $conn->connect_error);
+	}
+?>
 </body>
 </html>
+
