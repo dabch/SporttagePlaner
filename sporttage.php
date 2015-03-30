8bd2313ba0c5965cc6cc189a3effc93c3dbf1e21
Daniel Bücheler <dbahn25@t-online.de>
2015-03-30 20:38:14 +0200

    added new sporttage.php with links to both spiele.php and klassenlisten.php and advice on IE9 and HTML5

---
+++ sporttage.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta charset="UTF-8" name="viewport" content="width=device-width" />
+<title>Sporttage @ Kepler-Gymnasium</title>
+</head>
+<style>
+</style>
+
+<body>
+<h1> Willkommen bei der Sporttage-Webseite des Keplers!</h1>
+Da du es bis hierher geschafft hast, möchten wir dir Zugriff auf die folgenden Seiten geben:
+<br>
+<ul>
+	<li><a href="klassenlisten.php">Klassenlisten eintragen</a></li>
+	<li><a href="spiele.php">Spiele eintragen</a></li>
+</ul>
+<b>Bitte beachten: </b>Der Internet Explorer ist in Version 9 oder älter NICHT geeignet, außerdem wird HTML5 benötigt. Getestet wurde bisher mit Firefox 36.x <br>
+<br>
+Bei Problemen bitte persönlich an Sandesh oder Daniel wenden, oder per Email an <a href="mailto:admin@smv-kepler.de">admin@smv-kepler.de</a>
+<br>
